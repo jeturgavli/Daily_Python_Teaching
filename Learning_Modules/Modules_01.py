df4c699df3614e51f5140e64e996f2154d71aaba
jeturgavli <jeturgavli1in1@gmail.com>
2024-05-03 23:14:19 +0530

    Learning Module :P

---
+++ Learning_Modules/Modules_01.py
@@ -0,0 +1,9 @@
+#  module = a file containing python code. may contain functions, classes, etc
+# used with modular programing, which is to separate a program into parts
+
+import message as msg
+
+msg.helloo()
+msg.bye()
+msg.seeya()
+
---
+++ Learning_Modules/message.py
@@ -0,0 +1,8 @@
+def helloo():
+    print("Hello! Have a Good Day!")
+
+def bye():
+    print("Fuck you, Bye!")
+
+def seeya():
+    print("See Yaa Dude !!!!")
