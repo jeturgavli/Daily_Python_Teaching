bd7cb87885e88e8e14b53ef0136ffe0cdb678fab
jeturgavli <jeturgavli1in1@gmail.com>
2024-05-15 22:04:40 +0530

    Python Object Oriented Programing Added

---
+++ Python_Object_Oriented_Programing/01/01.py
@@ -0,0 +1,17 @@
+from car import Car
+
+car1 = Car("Chevy","Corvette", 2021, "Red")
+car2 = Car("Ford","Mustang", 2022, "Black")
+
+print(car2.make)
+print(car2.model)
+print(car2.year)
+print(car2.color)
+
+car2.drive()
+car2.stop()
+
+
+
+
+
